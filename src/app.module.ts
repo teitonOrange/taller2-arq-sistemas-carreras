2b35e0da6a0e956bee1ebf505a8996265029d567
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-12-13 22:03:13 -0300

    feat: remove unnused

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CareerController } from './career/career.controller';
+import { CareerModule } from './career/career.module';
+import { SubjectService } from './subject/subject.service';
+import { SubjectModule } from './subject/subject.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CareerModule, SubjectModule, PrismaModule],
 })
 export class AppModule {}
