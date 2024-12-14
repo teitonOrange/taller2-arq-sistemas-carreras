47b3a1abc2bf9b9697b6bd1ee55ace1addf3e65c
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-12-14 02:39:19 -0300

    feat: add subjects endpoints and services

--- src/subject/subject.controller.ts
+++ src/subject/subject.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { SubjectService } from './subject.service';
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller('subject')
 export class SubjectController {
     constructor(private readonly subjectService: SubjectService) {}
-
-    @Get()
+    @GrpcMethod('SubjectService', 'FindMany')
     async getAllSubjects(){
         return this.subjectService.findAll();
     }
+
+    @GrpcMethod('SubjectService', 'FindMapObjects')
+    async getMapObjects(){
+        return this.subjectService.findMapObjects();
+    }
+
+    @GrpcMethod('SubjectService', 'FindPreRequisite')
+    async getPreRequisite(){
+        return this.subjectService.findPreRequisite();
+    }
+
+    @GrpcMethod('SubjectService', 'FindPostRequisite')
+    async getPostRequisite(){
+        return this.subjectService.findPostRequisite();
+    }
+    
 }
--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,3 +1,4 @@
+import { tr } from '@faker-js/faker/.';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -6,6 +7,104 @@ export class SubjectService {
     constructor (private prisma: PrismaService) {}
     
     async findAll(){
-        return await this.prisma.subject.findMany();
+        const subjects =  await this.prisma.subject.findMany(
+            {
+                select: {
+                    code: true,
+                    name: true,
+                    department: true,
+                    credits: true,
+                    semester: true,
+                    id: true,
+                }
+
+            },
+        );
+        return {
+            subjects,
+        }
+    }
+
+    async findPreRequisite(){
+        const preRequisite = await this.prisma.subject.findMany(
+            {
+                select: {
+                    code: true,
+                    postrequisites: {
+                        select: {
+                            prerequisite: {
+                                select: {
+                                    code: true,
+                                }
+                            },
+                            subject: {
+                                select: {
+                                    code: true,
+                                }
+                            }
+                        }
+                    }
+                }   
+            }
+        );
+        
+        return {
+           subjectPreRequisites: preRequisite.map((subject) => {
+               return {
+                    subjectCode: subject.code,
+                    preRequisiteCodes: subject.postrequisites.map(
+                        (postrequisite) => {
+                          return postrequisite.prerequisite.code
+                          
+                        }
+                    )
+                }
+            })           
+        
+        }
+    }
+
+    async findPostRequisite(){
+        const preRequisite = await this.prisma.subject.findMany(
+            {
+                select: {
+                    code: true,
+                    prerequisites: {
+                        select: {
+                            prerequisite: {
+                                select: {
+                                    code: true,
+                                }
+                            },
+                            subject: {
+                                select: {
+                                    code: true,
+                                }
+                            }
+                        }
+                    }
+                }   
+            }
+        );
+        
+        return {
+           subjectPostRequisites: preRequisite.map((subject) => {
+               return {
+                    subjectCode: subject.code,
+                    postRequisiteCodes: subject.prerequisites.map(
+                        (postrequisite) => {
+                          return postrequisite.subject.code
+                          
+                        }
+                    )
+                }
+            })           
+        
+        }
+
+
+    
+
+
     }
 }
