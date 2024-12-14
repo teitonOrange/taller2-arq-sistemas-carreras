54ca58af4dfe44d210e677fde0be808aacc9f1a9
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-12-14 02:38:39 -0300

    feat: add microservice and grcp

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3002);
+  
+const app = await NestFactory.createMicroservice<MicroserviceOptions>
+(AppModule, {
+  transport: Transport.GRPC,
+    options: {
+      package: ['career', "subject"],
+        protoPath: [
+          join(__dirname, 'career/career.proto'),
+          join(__dirname, 'subject/subject.proto')
+        ],
+      url: 'localhost:5002',
+      },
+  });
+
+  await app.listen();
 }
 bootstrap();
